cb40a0bf67d8c2d21e4d583da4b00e742aae8758
Renat G <restspread@gmail.com>
2020-11-14 23:21:46 +0300

    feat: add block skills

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/* hamburger menu */
 const hamburger = document.querySelector('.hamburger'),
 			menu = document.querySelector('.menu'),
 			closeElem = document.querySelector('.menu__close');
@@ -9,11 +10,11 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
 });
+/*  */
 
 /* parallax */
 
 (function() {
-
 	document.addEventListener("mousemove", parallax);
 	const elem = document.querySelector(".promo");
 
@@ -24,14 +25,23 @@ closeElem.addEventListener('click', () => {
 			let _mouseY = e.clientY;
 			let _parallax1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
 			let _parallax2 = `${50 - (_mouseX - _w) * 0.002}% ${50 - (_mouseY - _h) * 0.01}%`;
-			let _parallax3 = `${50 - (_mouseX - _w) * 0.015}% ${50 - (_mouseY - _h) * 0.015}%`;
-			let _parallax4 = `${50 - (_mouseX - _w) * 0.05}% ${50 - (_mouseY - _h) * 0.05}%`;
-			let x = `${_parallax4}, ${_parallax3}, ${_parallax2}, ${_parallax1}`;
+			let _parallax3 = `${50 - (_mouseX - _w) * 0.001}% ${50 - (_mouseY - _h) * 0.01}%`;
+			let _parallax4 = `${50 - (_mouseX - _w) * 0.015}% ${50 - (_mouseY - _h) * 0.015}%`;
+			let _parallax5 = `${50 - (_mouseX - _w) * 0.04}% ${50 - (_mouseY - _h) * 0.04}%`;
+			let x = `${_parallax5}, ${_parallax4}, ${_parallax3}, ${_parallax2}, ${_parallax1}`;
 
 			elem.style.backgroundPosition = x;
 	}
-
 })();
+/*  */
 
+/* progress bar */
+
+const percent = document.querySelectorAll('.skills__progress-rating'),
+			lines = document.querySelectorAll('.skills__progress-line span');
+
+percent.forEach((item, i) => {
+	lines[i].style.width = item.innerHTML;
+});
 /*  */
 
